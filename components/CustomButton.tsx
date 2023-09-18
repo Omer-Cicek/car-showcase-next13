6e201ad7134307d15f05ede35ace216922a205a0
Omer-Cicek <omercicek160408@gmail.com>
2023-09-19 00:41:09 +0300

    Hero page completed

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -12,7 +12,7 @@ const CustomButton = ({
   return (
     <button
       disabled={false}
-      type={btnType}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
       onClick={() => handleClick}
     >
